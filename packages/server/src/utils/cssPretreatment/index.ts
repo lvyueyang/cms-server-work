2411b810455485cdefe76851398cc37d626e45ac
吕岳阳 <yueyang.lv@ucas.com.cn>
2023-04-20 15:40:17 +0800

    feat(css): css 预处理语言更换为 less 与 client-cms 统一

--- packages/server/src/utils/cssPretreatment/index.ts
+++ packages/server/src/utils/cssPretreatment/index.ts
@@ -1,26 +1,35 @@
 import * as chokidar from 'chokidar';
 import * as fs from 'fs';
+import * as less from 'less';
+import * as LessPluginAutoPrefix from 'less-plugin-autoprefix';
 import { join } from 'path';
-import * as sass from 'sass';
 
-const compileSass = (input: string, output: string) => {
-  sass.compileAsync(input, { style: 'compressed' }).then((res) => {
-    fs.writeFile(output, res.css, (err) => {
-      if (err) {
-        console.log('err: ', err);
-      } else {
-        console.log('编译成功', output);
-      }
+const autoprefixPlugin = new LessPluginAutoPrefix({
+  browsers: ['last 2 versions'],
+});
+
+const compile = (file_path: string, output: string) => {
+  const value = fs.readFileSync(file_path).toString();
+  less
+    .render(value, {
+      compress: true,
+      plugins: [autoprefixPlugin],
+    })
+    .then((res) => {
+      fs.writeFile(output, res.css, (err) => {
+        if (err) {
+          console.log('Less 编译失败: ', file_path, err);
+        }
+      });
     });
-  });
 };
 
 export function cssPretreatment(): void {
   chokidar
-    .watch(join(process.cwd(), 'views/static/styles/**/*.scss'))
-    .on('change', (e) => {
+    .watch(join(process.cwd(), 'views/static/styles/**/*.less'))
+    .on('change', (value) => {
       try {
-        compileSass(e, e.replace(/\.scss/, '.css'));
+        compile(value, value.replace(/\.less/, '.css'));
       } catch (e) {
         console.error(e);
       }
