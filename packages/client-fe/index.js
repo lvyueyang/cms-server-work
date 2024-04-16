8a26250b76f4a4e3f2de816effa1902fa5f18563
阿炸克斯 <975794403@qq.com>
2024-04-16 15:57:54 +0800

    fix: fe fix

--- packages/client-fe/index.js
+++ packages/client-fe/index.js
@@ -1,9 +1,13 @@
 const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
+const fse = require('fs-extra');
 const less = require('less');
 const LessPluginAutoPrefix = require('less-plugin-autoprefix');
 const chokidar = require('chokidar');
+
+const outputDir = path.join(__dirname, '/dist');
+console.log('outputDir: ', outputDir);
 
 const isProd = false;
 
@@ -74,28 +78,30 @@ function buildJs(module) {
   );
 }
 
-function buildStyle(module) {
-  const autoprefixPlugin = new LessPluginAutoPrefix({
-    browsers: ['last 2 versions'],
-  });
-  const compile = (file_path, output) => {
-    const value = fs.readFileSync(file_path).toString();
-    less
-      .render(value, {
-        compress: true,
-        plugins: [autoprefixPlugin],
-      })
-      .then((res) => {
-        fs.writeFile(output, res.css, (err) => {
-          if (err) {
-            console.log('Less 编译失败: ', file_path, err);
-          }
-        });
+const autoprefixPlugin = new LessPluginAutoPrefix({
+  browsers: ['last 2 versions'],
+});
+const compileLess = (file_path, output) => {
+  const value = fs.readFileSync(file_path).toString();
+  less
+    .render(value, {
+      compress: true,
+      plugins: [autoprefixPlugin],
+    })
+    .then((res) => {
+      fse.ensureFileSync(output);
+      fs.writeFile(output, res.css, (err) => {
+        if (err) {
+          console.log('Less 编译失败: ', file_path, err);
+        }
       });
-  };
-  compile(
+    });
+};
+
+function buildStyle(module) {
+  compileLess(
     path.join(__dirname, '/src/pages/' + module + '/style.less'),
-    path.join(__dirname, '/dist/pages/' + module + '/style.css'),
+    path.join(outputDir, '/pages/' + module + '/style.css'),
   );
 }
 
@@ -107,7 +113,20 @@ function build(module) {
 // build('home');
 chokidar.watch(path.join(__dirname, '/src/**/*')).on('change', (value) => {
   try {
-    console.log('value: ', value);
+    const key = path.join(__dirname, '/src/pages/');
+    // 页面模块的特殊处理
+    if (value.startsWith(key)) {
+      const module = value.split(key)[1].split(path.join('/'))[0];
+      build(module);
+    } else {
+      if (path.parse(value).ext === '.less') {
+        const s = path.join(__dirname, '/src');
+        const o = value.split(s)[1].replace(/\.less/, '.css');
+        const t = path.join(outputDir, o);
+        console.log('t: ', t);
+        compileLess(value, t);
+      }
+    }
     // compile(value, value.replace(/\.less/, '.css'));
   } catch (e) {
     console.error(e);
--- packages/client-fe/src/pages/home/main.ts
+++ packages/client-fe/src/pages/home/main.ts
@@ -1,1 +1,1 @@
-console.log('hello world1');
+console.log('hello world11');
