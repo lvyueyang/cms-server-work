5f92b3f6db738cb09600cc7ff55d9aa8f17e8fbb
lvyueyang <975794403@qq.com>
2025-11-12 16:36:29 +0800

    chore: 移除前端SSR相关代码及依赖

--- clients/fe/src/typing.ts
+++
@@ -1,6 +0,0 @@
-type PromiseReturnType<T extends Promise<any>> = T extends Promise<infer R> ? R : never;
-
-export interface SSRProps<T extends (...args: any) => any> {
-  pageData: PromiseReturnType<ReturnType<T>>;
-  globalData: any;
-}
